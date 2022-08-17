0c9b0d1e15fe6aae1d847fef8f5dd80d8fb61e8d
Sebastian Thomas <mail@sthomas.ch>
2022-08-17 14:31:36 +0200

    Cleanup ProductAtStation

--- src/stations/ProductAtStation.ts
+++ src/stations/ProductAtStation.ts
@@ -1,20 +1,5 @@
-import { Model } from 'sequelize'
 import { ID as IDType } from '../ID'
 import { StationNameType } from './Station'
-
-/**
- * Type `I` = type of `ProductID`s
- */
-class ProductAtStationModel<I extends IDType> extends Model {
-  constructor(
-    public stationName: StationNameType,
-    public productID: I,
-    public lastPacked: Date,
-    public countPacked: number,
-  ) {
-    super()
-  }
-}
 
 type ProductAtStationType<I extends IDType> = {
   stationName: StationNameType
