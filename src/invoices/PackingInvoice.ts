cdd65ce5cd6e701aaf959e051de26d47a6018e8d
Sebastian Thomas <mail@sthomas.ch>
2022-08-17 16:41:23 +0200

    Fix PackingInvoice Status

--- src/invoices/PackingInvoice.ts
+++ src/invoices/PackingInvoice.ts
@@ -3,6 +3,6 @@ import { ID as IDType } from '../ID'
 type PackingInvoiceType<I extends IDType, StatusType extends string[] | readonly string[]> = {
   invoiceID: I
   currentStation: string
-  packingStatus: StatusType
+  packingStatus: StatusType[number]
 }
 export default PackingInvoiceType
