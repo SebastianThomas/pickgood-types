68f1c4538b9bad6dd82597a54173b485b1b69d2b
Sebastian Thomas <mail@sthomas.ch>
2022-08-22 09:47:08 +0200

    Added UserRank!

--- src/User.ts
+++ src/User.ts
@@ -1,10 +1,16 @@
 import { ID as IDType } from './ID'
 import { StationNameType } from './stations/Station'
 
+export enum UserRank {
+  Administrator = 'Administrator',
+  User = 'User',
+}
+
 type UserType<I extends IDType> = {
   userID: I
   firstName: string
   lastName: string
   stationName: StationNameType | null
+  rank: UserRank
 }
 export default UserType
