57d87a9627371e597bbd3d548ba5b2e324fac42a
Sebastian Thomas <mail@sthomas.ch>
2022-08-26 18:34:50 +0200

    Added PackingInvoiceStatus

--- src/invoices/PackingInvoice.ts
+++ src/invoices/PackingInvoice.ts
@@ -1,8 +1,20 @@
 import { ID as IDType } from '../ID'
 
+const packingInvoiceStatus = [
+  'Pending',
+  'QueuedAtStation',
+  'PackingAtStation',
+  'Question',
+  'ProblemOrQuestion',
+] as const
+
+type PackingInvoiceStatusPossibilitiesType = typeof packingInvoiceStatus
+type PackingInvoiceStatusType = typeof packingInvoiceStatus[number]
+
 type PackingInvoiceType<I extends IDType, StatusType extends string[] | readonly string[]> = {
   invoiceID: I
   currentStation: string
   packingStatus: StatusType[number]
 }
 export default PackingInvoiceType
+export { packingInvoiceStatus, PackingInvoiceStatusPossibilitiesType, PackingInvoiceStatusType }
